6fe5c3a9e2492b74239f12bd39ae1582f601c63b
unknown <subhamraj4114@gmail.com>
2020-10-13 23:50:54 +0530

    added icon and changed title of the website

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,17 +1,45 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 
 function Header() {
+    const hamburger = useRef(null);
+    const linkContainer = useRef(null);
+
+    const toggleClass = () => {
+        hamburger.current.classList.toggle("is-active");
+        linkContainer.current.classList.toggle("is-active");
+    }
+
+    useEffect(() => {
+        let timer;
+        const handleResize = () => {
+            clearTimeout(timer);
+            timer = setTimeout(() => {
+                hamburger.current.classList.remove("is-active");
+                linkContainer.current.classList.remove("is-active");
+            }, 300);
+        }
+        window.addEventListener("resize", handleResize)
+        return () => {
+            window.removeEventListener("resize", handleResize)
+        }
+    }, [])
+
     return (
         <header>
             <div className="logo">Hacker News</div>
-            <ul>
-                <li>New</li>
-                <li> Past</li>
-                <li>Comments</li>
-                <li>Ask</li>
-                <li>Show</li>
-                <li>Jobs</li>
-                <li>Submit</li>
+            <div onClick={toggleClass} ref={hamburger} className="hamburger" id="hamburger-6">
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+            </div>
+            <ul ref={linkContainer}>
+                <li onClick={toggleClass}>New</li>
+                <li onClick={toggleClass}>Past</li>
+                <li onClick={toggleClass}>Comments</li>
+                <li onClick={toggleClass}>Ask</li>
+                <li onClick={toggleClass}>Show</li>
+                <li onClick={toggleClass}>Jobs</li>
+                <li onClick={toggleClass}>Submit</li>
             </ul>
         </header>
     );
